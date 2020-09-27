88f9d0c2289fddd528320bbe46e572c0c54a1a8e
Yihan Dang <qcloud1014@gmail.com>
2020-09-27 22:51:17 +0800

    write function skeleton

--- src/NFdeps.c
+++ src/NFdeps.c
@@ -1,2 +1,9 @@
 /* Examine the dependency of a given so, and write it into somewhere in the link map
     may also configure the search path of it */
+
+#include "../NFlink.h"
+
+void map_deps(struct NF_link_map *l)
+{
+
+}
