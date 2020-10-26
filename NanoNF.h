1853ef3106905eac082c8ac5afcc475a0a146b21
Yihan Dang <qcloud1014@gmail.com>
2020-10-27 00:50:50 +0800

    expose NFusage_worker, will change this later

--- NanoNF.h
+++ NanoNF.h
@@ -4,3 +4,4 @@
 extern void *NFopen(const char* file, int mode, void *addr);
 extern void *NFsym(void *l, const char *s);
 extern uint64_t NFusage(void *l);
+extern uint64_t NFusage_worker(const char *name, int mode);
